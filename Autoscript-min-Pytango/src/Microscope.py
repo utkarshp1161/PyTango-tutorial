4298e9c14d48f58ba65bcd9599df6aeb979b8d79
utkarshp1161 <utkarshp1161@gmail.com>
2026-02-20 19:42:06 -0500

    add the abstraction for AS and pytango

---
+++ Autoscript-min-Pytango/src/Microscope.py
@@ -0,0 +1,21 @@
+from tango.server import Device, command, attribute, device_property
+from 
+
+
+class Microscope(Device):
+    # use device_prpert u fo rip address and port
+    
+    # define attributes - with untis and evreything - good to use decorator
+    
+    
+    # init function
+    
+    # define command  connect microscope - Autoscript 
+    
+    # define command  discnnect mic - Autoscript 
+    
+    # define command - get_image(in is str of detector name, out is flattened array)
+        # use the device proxy of haadf to set 
+        # acquire -> 
+
+    # get EDS
---
+++ Autoscript-min-Pytango/src/detectors/HAADF.py
@@ -0,0 +1,14 @@
+## HAADF detector
+from tango.server import Device, command, attribute
+
+
+
+class HAADF(Device):
+    
+    # define attributes - with untis and evreything - good to use decorator, Activate 
+    
+    
+    # init function
+    
+    # no commands as such for now
+    
